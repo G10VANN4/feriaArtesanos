cac20b6bfd2958b62c0dc88b19031bd4ea5c2ceb
luunf <delialu681@gmail.com>
2025-10-22 16:08:50 +0100

    Update artesano.py

--- backend/models/artesano.py
+++ backend/models/artesano.py
@@ -5,7 +5,6 @@ class Artesano(db.Model):
     
     artesano_id = db.Column(db.Integer, primary_key=True)
     usuario_id = db.Column(db.Integer, db.ForeignKey('Usuario.usuario_id'), nullable=False, unique=True)
-    email = db.Column(db.String(100), nullable=False, unique=True)
     dni = db.Column(db.String(8), nullable=False, unique=True)
     telefono = db.Column(db.String(20), nullable=False)
     fecha_registro = db.Column(db.DateTime, default=db.func.current_timestamp())
@@ -15,7 +14,6 @@ class Artesano(db.Model):
         return {
             'artesano_id': self.artesano_id,
             'usuario_id': self.usuario_id,
-            'email': self.email,
             'dni': self.dni,
             'telefono': self.telefono,
             'fecha_registro': self.fecha_registro.isoformat() if self.fecha_registro else None
