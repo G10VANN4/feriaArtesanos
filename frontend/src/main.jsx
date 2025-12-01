d344387104c7444e2f67a36cffca1fbab19906b7
luunf <delialu681@gmail.com>
2025-12-02 00:30:14 +0100

    Revert "google (solo desde google) no en nuestra web"
    This reverts commit 19f57910d6e9edaca921fdc155b79cbad050c45a.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,24 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import ReactGA from 'react-ga4';
 import './index.css'
+
 import App from './App.jsx'
-
-// CONFIGURACIÓN ALTERNATIVA que usa el endpoint de Google directamente
-ReactGA.initialize('G-CZE9J6QW0D', {
-  gtagOptions: {
-    debug_mode: true,
-    // Esta línea puede ayudar a evitar bloqueadores básicos
-    transport_type: 'beacon'
-  },
-  // Fuerza el uso de la vía estándar
-  testMode: false
-});
-
-ReactGA.send('pageview');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 )
