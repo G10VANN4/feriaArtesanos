226094400f8ec27baaf965fe860a42a19bf6d9a3
luunf <delialu681@gmail.com>
2025-12-02 10:38:24 +0100

    Google Analytics solo por web

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactGA from 'react-ga4';
 import './index.css'
-
 import App from './App.jsx'
+
+const GA_MEASUREMENT_ID = 'G-CZE9J6QW0D'; //modificar
+
+ReactGA.initialize(GA_MEASUREMENT_ID, { 
+  gtagOptions: {
+    debug_mode: true,
+    transport_type: 'beacon'
+  },
+  testMode: false
+});
+
+ReactGA.send('pageview');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
